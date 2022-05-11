109b9d19c18d9471b5fb8a682987b7dce44ebc22
anandpatel48 <anandrpatel48@gmail.com>
2022-05-11 11:29:05 -0400

    backend is done

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+
+
+const projects = require("./projects.json");
+const about = require("./about.json");
+
+const app = express()
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+app.get("/projects", (req, res) => {
+    res.json(projects);
+  });
+  
+
+  app.get("/about", (req, res) => {
+    res.json(about);
+  });
+
+  
+const PORT = 4000
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
